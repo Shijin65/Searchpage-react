94b6e7750def5f76e8a5f57473e6204c046071ec
shijin65 <shijincht65@gmail.com>
2023-09-06 21:04:24 +0530

    fetched data from the place holder api

--- react_searchbar/src/componets/Search/SearchIn.jsx
+++ react_searchbar/src/componets/Search/SearchIn.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './SearchIn.css'
 import {FaSearch} from 'react-icons/fa';
-export const SearchIn = (props) => {
-  const color=props.colour
 
-  const fetchdata = ()=>{}
+
+export const SearchIn = (props) => {
+  const [text,settext]=useState("");
+  const color=props.colour
+// console.log(text)
+const fetchdata =(value)=>{
+  fetch("https://jsonplaceholder.typicode.com/users")
+  .then((responce)=>responce.json())
+  .then((json)=>{
+    console.log(json)
+  } )}
+
+const handlechange = (value) => {
+  settext(value)
+  fetchdata(value)
+}
+ 
   return (
     <div className='input_wrap'>
         <FaSearch id='search_icon' style={{color:color}}/>
-        <input type='text' placeholder='You Can Start Here'></input>
+        <input type='text' placeholder='You Can Start Here'
+        value={text}
+        onChange={(e)=> handlechange(e.target.value)}></input>
+        
     </div>
   )
 }
