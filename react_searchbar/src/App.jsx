d5ccf4f407f97773ae7968b08c9207caec140322
shijin65 <shijincht65@gmail.com>
2023-09-06 14:42:23 +0530

    new sample page for geting suggusion on the search bar

---
+++ react_searchbar/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState,useEffect } from 'react'
+
+import './App.css'
+import { SearchIn } from './componets/SearchIn';
+
+function App() { 
+  var x = Math. floor(Math. random() * 256);
+  var y = Math. floor(Math. random() * 256);
+  var z = Math. floor(Math. random() * 256);
+  var colour = "rgb(" + x + "," + y + "," + z + ")";
+  console.log(colour)
+
+  return (
+   <div className='App' style={{backgroundColor:colour}} >
+    <h1>Search Bar Using API</h1>
+    <div className='searchbar_container'>
+        <SearchIn/>
+        <div>Suggestion</div>
+    </div>
+   </div>
+  )
+}
+
+export default App
